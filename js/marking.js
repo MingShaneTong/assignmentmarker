14cb4c73dbac38a966eef0332652f62b58e943c7
MingShaneTong <mstong800@gmail.com>
2021-04-14 09:54:28 +1200

    Main Functionality

--- js/marking.js
+++ js/marking.js
@@ -2,7 +2,16 @@ function addCriterionListeners(){
 	document.querySelectorAll('.criterion').forEach(item => {
 		$(item).find('input[type="checkbox"]').on("click", onCheck);
 		$(item).find('input[type="range"]').on("change", onSlide);
+		$(item).find('input[type="range"]').on("mouseover", onSlideOver);
 	})
+}
+
+function getGradeBox(){
+	return $("#grade");
+}
+
+function getCommentBox(){
+	return $("#comment");
 }
 
 /**
@@ -46,45 +55,97 @@ function onSlide(evt){
 }
 
 /**
+ * When slider is hovered over, create a hovering box that displays the value
+ */
+function onSlideOver(evt){
+	var slider = $(evt.target);
+	var position = slider.position();
+	var width = slider.width();
+	var offset = width * slider.val() / slider.attr("max");
+	var left = position.left + offset;
+
+	// place box over
+	$("#slider-hover").css({
+		"top": position.top,
+		"left": left,
+		"display": "block"
+    });
+}
+
+/**
  * Generate the grade and comment
  */
 function processSchedule(){
+	getGradeBox().val('');
+	getCommentBox().text('');
+
 	var categories = $("#file-content").find(".category");
 	var total = 0;
 	for(var i = 0; i < categories.length; i++){
 		// find the criterion from the category
 		var category = $(categories[i]);
-		var criteria = category.children(".criterion");
-		console.log(criteria);
-		for(var j = 0; j < criteria.length; j++){
-			total += processCriterion($(criteria[j]))-0;
-		}
+		total += processCategory(category)-0;
 	}
-	console.log(total);
+	getGradeBox().val(total);
+}
+
+/**
+ * Returns the points awarded for the category
+ *
+ * @param {$(Category)}	category 	The element of the category
+ * @return {int}	points awarded to the category
+ */
+function processCategory(category){
+	getCommentBox().append(category.find("h3").text() + " - ");
+
+	var criteria = category.children(".criterion");
+	var max = 0;
+	var total = 0;
+	var comments = [];
+	// get total, max and comments from each criterion
+	for(var j = 0; j < criteria.length; j++){
+		var critresult = processCriterion($(criteria[j]));
+		total += critresult[0]-0;
+		max += critresult[1]-0;
+		comments.push(critresult[2]);
+	}
+
+	getCommentBox().append(total + "/" + max +"\n");
+	// print the comments
+	for(var i = 0; i < comments.length; i++) {
+		getCommentBox().append(comments[i] +"\n");
+	}
+	getCommentBox().append("\n");
+
+	return total;
 }
 
 /**
  * Returns the points awarded for the criteria
  *
  * @param {$(Criterion)}	criterion 	The element of the criterion
- * @return {int}	points awarded to the criterion
+ * @return {int, int, String}	points awarded, maximum points, comment
  */
 function processCriterion(criterion){
 	var checkbox = criterion.find("input[type=\"checkbox\"]");
+	var critTitle = criterion.find("p");
 	var range = criterion.find("input[type=\"range\"]");
+	var max = range.attr("max");
 
+	// check the grade of the criterion
 	if(checkbox.attr("data-indeterminate") == "false"){
 		// award full points or no points
 		if(checkbox.prop("checked"))
-			return range.attr("max");
+			return [max, max, "Completed "+critTitle.text()];
 		else
-			return 0;
+			return [0, max, "No attempt at "+critTitle.text()];
 	} 
 	// check slider for partial marks
-	return range.val();
+	return [range.val(), max, "Some attempt at "+critTitle.text()];
 }
 
 
 function onScheduleLoad(){
 	addCriterionListeners();
+	$("#grade-comments").css("display", "block");
 }
